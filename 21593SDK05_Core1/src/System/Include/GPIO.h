2fe73b4682692489d329f1f1bb2eb94de2149076
prashanth <prashanth.pipala@jasmin-infotech.com>
2025-06-24 13:29:27 +0200

    LED update

--- 21593SDK05_Core1/src/System/Include/GPIO.h
+++ 21593SDK05_Core1/src/System/Include/GPIO.h
@@ -23,13 +23,21 @@ int Led_Toggle_Count2=0;
 
 #define Num_Of_Callbacks	2
 
-#define LED1             	"LED1"
-#define LED1_PORT      		(ADI_GPIO_PORT_C)
-#define LED1_PIN       		(ADI_GPIO_PIN_3)
+//#define LED1             	"LED1"
+//#define LED1_PORT      		(ADI_GPIO_PORT_C)
+//#define LED1_PIN       		(ADI_GPIO_PIN_3)
+//
+//#define LED2             	"LED2"
+//#define LED2_PORT      		(ADI_GPIO_PORT_C)
+//#define LED2_PIN       		(ADI_GPIO_PIN_2)
 
-#define LED2             	"LED2"
-#define LED2_PORT      		(ADI_GPIO_PORT_C)
-#define LED2_PIN       		(ADI_GPIO_PIN_2)
+#define LED1                "LED1"
+#define LED1_PORT           (ADI_GPIO_PORT_B)
+#define LED1_PIN            (ADI_GPIO_PIN_5)
+
+#define LED2                "LED2"
+#define LED2_PORT           (ADI_GPIO_PORT_B)
+#define LED2_PIN            (ADI_GPIO_PIN_8)
 
 
 #endif /* GPIO_H_ */
